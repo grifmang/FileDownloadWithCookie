ca1596edce83b330ced3909ecf523952ccf113bb
grifmang <grifmang@gmail.com>
2014-11-28 23:44:59 -0500

    Bug Fixes

--- getFiles.py
+++ getFiles.py
@@ -2,7 +2,7 @@ import urllib2
 import urllib
 from cookielib import CookieJar
 
-files = 'http://www.nytimes.com/interactive/2014/11/25/us/evidence-released-in-michael-brown-case.html'
+files = 'http://www.nytimes.com/interactive/2014/11/25/us/evidence-released-in-michael-brown-case.html?_r=0'
 slashpos = 0
 
 def getLinks(url):
@@ -13,10 +13,8 @@ def getLinks(url):
     for line in p:
         for element in line.split():
             if element.startswith('href="http://gr'):
-                    if element.endswith('pdf"') or element.endswith('png"') or element.endswith('jpg"'):
-                        result.append(element[6:])
-                    else:
-                        continue
+                if element.endswith('pdf"') or element.endswith('png"') or element.endswith('jpg"'):
+                    result.append(element[6:])
     for char in result:
         slashpos = char.rfind('/') + 1
         urllib.urlretrieve(char, char[slashpos:-1])
